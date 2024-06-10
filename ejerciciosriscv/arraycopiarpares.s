4e5a80b3a5c6e733fb4c4b7fe2e81191264ac26a
Ivan Berrutto <ivanberrutto@gmail.com>
2024-06-10 15:11:57 -0300

    termino ejercicio 2 copiar pares

--- ejerciciosriscv/arraycopiarpares.s
+++ ejerciciosriscv/arraycopiarpares.s
@@ -3,8 +3,8 @@
 # pares de q a s y si no 0.
 
 .text:
-la a0, input_array1
-la a1, input_array2
+la a0, input_array1 # array donde voy a guardar
+la a1, input_array2 # array que voy a copiar
 la a2, array_length
 jal ra, copiarpares
 # Imprime el resultado.
@@ -17,27 +17,30 @@ li a7, 10
 ecall
 
 copiarpares:
-    addi a2, a2, -1
+    #addi a2, a2, -1
     
     ciclo:
         bge zero , a2 , fin
         lw t0, 0(a1)    # De-referencia la dir,
-		addi   a0, a0, 4   # Actualiza la dir. de memoria
-		addi   a1, a1, 4   # Actualiza la dir. de memoria
-		addi  a2, a2, -1    # decrementa el tamanio
-
         addi t1 , zero , 1 #preparo la mascara 0x1
         and t1, t0 , t1
         beq t1, zero, espar
         sw zero, 0(a0)
+    espar_continuar:
+		addi   a0, a0, 4   # Actualiza la dir. de memoria
+		addi   a1, a1, 4   # Actualiza la dir. de memoria
+		addi  a2, a2, -1    # decrementa el tamanio
+
+
+
 		j ciclo           # Vuelve a comenzar el ciclo
     fin:
         ret
     espar:
         sw t0, 0(a0)
-        j ciclo
+        j espar_continuar
 
 .data:
 .equ array_length, 4 
-input_array2: .word 0x12345678  0x11111111  0x8  0x1
-input_array1: .word 0x00000000  0x00000000  0x00000000  0x00000000  
+input_array2: .word 0x1  0x2  0x3  0x4
+input_array1: .word 0x00000400  0x00000400  0x00000400  0x00000400  
